69653574f96dba29679705a7ea8ebc83a58b4bc9
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2021-02-08 04:22:49 +0600

    settings for ui

--- HaateKhari/settings.py
+++ HaateKhari/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'lx^ie4_wxy)qr6$gj8(h62rqxe&+y1o)cv6-8y3-3qclwv2qmw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.105', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
